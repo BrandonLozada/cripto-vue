4dce5847d2f18265e731818358b531c33efa709a
BrandonLozada <brandon_lozada@outlook.com>
2023-06-20 10:32:07 -0600

    Agregar Alerta; Eliminar componente obsoleto.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
+import Alerta from './components/Alerta.vue'
 
 const monedas = ref([
       { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
@@ -9,6 +10,11 @@ const monedas = ref([
     ])
 
 const criptomonedas = ref([])
+const error = ref('')
+const cotizar = reactive({
+  moneda: '',
+  criptomoneda: ''
+})
 
 onMounted(() => {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
@@ -16,6 +22,17 @@ onMounted(() => {
     .then(respuesta => respuesta.json())
     .then(({Data}) => criptomonedas.value = Data)
 })
+
+const cotizarCripto = () => {
+  // Validar que cotizar esté lleno
+  if (Object.values(cotizar).includes('')) {
+    error.value = 'Todos los campos son obligatorios'
+    return
+  }
+
+  error.value = ''
+
+}
 </script>
 
 <template>
@@ -23,10 +40,19 @@ onMounted(() => {
     <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
       <div class="contenido">
 
-          <form class="formulario">
+        <Alerta
+          v-if="error"
+        >{{ error }}</Alerta>
+          <form
+            class="formulario"
+            @submit.prevent="cotizarCripto"
+            >
             <div class="campo">
               <label for="moneda">Moneda:</label>
-              <select id="moneda">
+              <select
+                id="moneda"
+                v-model="cotizar.moneda"
+              >
                 <option value="">-- Selecciona --</option>
                 <option
                   v-for="moneda in monedas"
@@ -38,7 +64,10 @@ onMounted(() => {
 
             <div class="campo">
               <label for="cripto">Cripto:</label>
-              <select id="cripto">
+              <select
+                id="cripto"
+                v-model="cotizar.criptomoneda"
+              >
                 <option value="">-- Selecciona --</option>
                 <option
                   v-for="criptomoneda in criptomonedas"
